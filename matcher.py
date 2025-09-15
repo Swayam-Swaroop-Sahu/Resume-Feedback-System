06f9fed423e3c458c1bcc44d6ec5e5935e0959b5
wizardofvoid <sarafa736@gmail.com>
2025-09-15 18:58:00 +0530

    Cleaning

--- matcher.py
+++
@@ -1,9 +0,0 @@
-def calculate_skill_match(resume_skills, job_skills):
-    matched = set(resume_skills) & set(job_skills)
-    if not job_skills:
-        return 0
-    return (len(matched)/len(job_skills)) * 100
-
-def calculate_ats_score(skill_match, has_contact_info):
-    score = (skill_match*0.8) + (has_contact_info * 20)
-    return min(score, 100)
